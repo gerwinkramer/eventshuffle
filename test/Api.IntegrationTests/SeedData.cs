ced0888cebc1446ee2d95a17dd327a3c7aff66c6
Gerwin Kramer <gerwin.kramer@vincit.fi>
2020-08-31 14:43:07 +0300

    Fix running tests in parallel

--- test/Api.IntegrationTests/SeedData.cs
+++ test/Api.IntegrationTests/SeedData.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Eventshuffle.Domain.Events;
 using Eventshuffle.Infrastructure.Persistence;
 using NodaTime;
@@ -7,11 +8,28 @@ namespace Eventshuffle.Api.IntegrationTests
 {
     public class SeedData
     {
+        private static readonly object _lock = new object();
+
         public static void Populate(EventshuffleDbContext context)
         {
-            context.Events.Add(new EventEntity("Name1", new List<LocalDate> { new LocalDate(2000, 01, 01)}));
-            context.Events.Add(new EventEntity("Name2", new List<LocalDate>()));
-            context.SaveChanges();
+            lock (_lock)
+            {
+                if (!context.Events.Any())
+                {
+                    context.Events.Add(new EventEntity(
+                        "Name1",
+                        new List<LocalDate>
+                        {
+                            new LocalDate(2000, 01, 01)
+                        }));
+
+                    context.Events.Add(new EventEntity(
+                        "Name2", 
+                        new List<LocalDate>()));
+
+                    context.SaveChanges();
+                }
+            }
         }
     }
 }
